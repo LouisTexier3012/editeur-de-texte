a9954fde609ca6da4988c4de8919a674c9d3215c
texierl <louis.texpro@gmail.com>
2023-11-08 08:35:38 +0100

    pas finit remplacer

---
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -0,0 +1,25 @@
+package fr.iut.editeur.commande;
+
+import fr.iut.editeur.document.Document;
+
+public class CommandeRemplacer extends CommandeDocument {
+
+
+    public CommandeRemplacer(Document document, String[] parameters) {
+        super(document, parameters);
+    }
+
+    @Override
+    public void executer() {
+        if(parameters.length < 4) {
+            System.err.println("Format attendu : remplacer;depart;fin;chaine");
+            return;
+        }
+        String texte = parameters[4];
+        int depart = Integer.parseInt(parameters[2]);
+        int fin = Integer.parseInt(parameters[3]);
+        this.document.ajouter(texte);
+        super.executer();
+    }
+
+}

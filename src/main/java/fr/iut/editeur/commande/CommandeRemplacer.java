38791db2ee65d26a237a4da825c261cb3356574e
texierl <louis.texpro@gmail.com>
2023-11-08 08:36:52 +0100

    finit remplacer

--- src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
+++ src/main/java/fr/iut/editeur/commande/CommandeRemplacer.java
@@ -18,7 +18,7 @@ public class CommandeRemplacer extends CommandeDocument {
         String texte = parameters[4];
         int depart = Integer.parseInt(parameters[2]);
         int fin = Integer.parseInt(parameters[3]);
-        this.document.ajouter(texte);
+        this.document.remplacer(depart,fin,texte);
         super.executer();
     }
 

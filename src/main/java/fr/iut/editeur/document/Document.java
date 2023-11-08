a9f2c3ac72ba5075170bedf5feb270b5cdc81fa0
texierl <louis.texpro@gmail.com>
2023-11-08 09:16:46 +0100

    effacer check

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -34,4 +34,7 @@ public class Document {
     public void majuscules(int start, int end) {
         remplacer(start,end,texte.substring(start,end).toUpperCase());
     }
+    public void effacer(int start, int end) {
+        remplacer(start,end,"");
+    }
 }

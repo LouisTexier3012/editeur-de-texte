b29bfb80d5d185391027b6fb672ff96a7fbe2c83
texierl <louis.texpro@gmail.com>
2023-11-09 11:15:03 +0100

    nul:changement inutille

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -2,49 +2,50 @@ package fr.iut.editeur.document;
 
 public class Document {
 
-    private String texte;
+    private String texteDocument;
 
     public Document() {
-        this.texte = "";
+        this.texteDocument = "";
     }
 	
-    public String getTexte() {
-        return texte;
+    public String getTexteDocument() {
+        return texteDocument;
     }
 
-    public void setTexte(String texte) {
-        this.texte = texte;
+    public void setTexteDocument(String texteDocument) {
+        this.texteDocument = texteDocument;
     }
 
     public void ajouter(String texte) {
-        this.texte += texte;
+        this.texteDocument += texte;
     }
 
     @Override
     public String toString() {
-        return this.texte;
+        return this.texteDocument;
     }
 
     public void remplacer(int start, int end, String grandRemplacement) {
-        String leftPart = texte.substring(0, start);
-        String rightPart = texte.substring(end);
-        texte = leftPart + grandRemplacement + rightPart;
+        String leftPart = texteDocument.substring(0, start);
+        String rightPart = texteDocument.substring(end);
+        texteDocument = leftPart + grandRemplacement + rightPart;
     }
 
     public void majuscules(int start, int end) {
-        remplacer(start,end,texte.substring(start,end).toUpperCase());
+        remplacer(start,end, texteDocument.substring(start,end).toUpperCase());
     }
 
-    public void minuscules(int start, int end){ remplacer(start,end,texte.substring(start,end).toLowerCase());}
+    public void minuscules(int start, int end){ remplacer(start,end, texteDocument.substring(start,end).toLowerCase());}
 
     public void effacer(int start, int end) {
         remplacer(start,end,"");
     }
 
     public void inserer(int indice, String grandReplacement){
-        String leftPart = texte.substring(0,indice+1);
-        String rightPart = texte.substring(indice+1);
-        texte = leftPart + grandReplacement + rightPart;
+        String leftPart = texteDocument.substring(0,indice+1);
+        String rightPart = texteDocument.substring(indice+1);
+        texteDocument = leftPart + grandReplacement + rightPart;
     }
-    public void clear() {texte="";}
+    public void clear() {
+        texteDocument ="";}
 }

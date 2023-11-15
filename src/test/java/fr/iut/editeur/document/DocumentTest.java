8aac594ec6213fbc707781e520b23d4a9c189e8a
texierl <louis.texpro@gmail.com>
2023-11-15 09:47:28 +0100

    test:yml

--- src/test/java/fr/iut/editeur/document/DocumentTest.java
+++ src/test/java/fr/iut/editeur/document/DocumentTest.java
@@ -6,50 +6,50 @@ public class DocumentTest extends TestCase {
 
     public void testAjouter() {
         Document document = new Document();
-        document.ajouter("Hello");
-        assertEquals("Hello", document.getTexte());
+        document.setTexteDocument("Hello");
+        assertEquals("Hello", document.getTexteDocument());
     }
 
     public void testRemplacer() {
         Document document = new Document();
-        document.setTexte("Hello, world!");
+        document.setTexteDocument("Hello, world!");
         document.remplacer(7, 12, "Universe");
-        assertEquals("Hello, Universe!", document.getTexte());
+        assertEquals("Hello, Universe!", document.getTexteDocument());
     }
 
     public void testMajuscules() {
         Document document = new Document();
-        document.setTexte("hello");
+        document.setTexteDocument("hello");
         document.majuscules(0, 4);
-        assertEquals("HELLo", document.getTexte());
+        assertEquals("HELLo", document.getTexteDocument());
     }
 
     public void testMinuscules() {
         Document document = new Document();
-        document.setTexte("HELLO");
+        document.setTexteDocument("HELLO");
         document.minuscules(0, 4);
-        assertEquals("hellO", document.getTexte());
+        assertEquals("hellO", document.getTexteDocument());
     }
 
     public void testEffacer() {
         Document document = new Document();
-        document.setTexte("Hello, world!");
+        document.setTexteDocument("Hello, world!");
         document.effacer(5, 12);
-        assertEquals("Hello!", document.getTexte());
+        assertEquals("Hello!", document.getTexteDocument());
     }
 
     public void testInserer() {
         Document document = new Document();
-        document.setTexte("Hello!");
+        document.setTexteDocument("Hello!");
         document.inserer(4, " world");
-        assertEquals("Hello world!", document.getTexte());
+        assertEquals("Hello world!", document.getTexteDocument());
     }
 
     public void testClear() {
         Document document = new Document();
-        document.setTexte("Hello");
+        document.setTexteDocument("Hello");
         document.clear();
-        assertEquals("", document.getTexte());
+        assertEquals("", document.getTexteDocument());
     }
 }
 
